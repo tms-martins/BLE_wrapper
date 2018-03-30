af92fc17108f81803b6a7822601f1ffcfa843894
Christian <6766478+Wetterprophet@users.noreply.github.com>
2018-03-30 12:24:16 +0200

    add
    added tiago as co-author
    
    Co-Authored-By: Tiago M. <tms-martins@users.noreply.github.com>

--- BLE_wrapper.ino
+++ BLE_wrapper.ino
@@ -5,7 +5,6 @@
 #include <BLE2902.h>
 #include <BLEUUID.h>
 
-
 // forward declaration, so we can call the function in setup() or loop()
 // before the actual body is declared (the function's body is in another tab)
 void setupJacketBLE();
